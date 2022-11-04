2ca6337b2aade3c78dcd19c13785657ec351dfe9
Farid Maulana <52988155+farid-maulana@users.noreply.github.com>
2022-11-04 11:03:58 +0700

    Create master template for admin role

---
+++ resources/views/layouts/admin/master.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookStore</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/iconly/bold.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendors/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.svg') }}" type="image/x-icon">
+    <!-- Custom Page Stye -->
+    @stack('style')
+</head>
+
+<body>
+    <div id="app">
+        <!-- start: SIDEBAR -->
+        @include('layouts.admin.partials.sidebar')
+        <!-- end: SIDEBAR -->
+
+        <div id="main">
+            <!-- start: CONTENT -->
+            @yield('content')
+            <!-- end: CONTENT -->
+
+            <!-- start: FOOTER -->
+            @include('layouts.admin.partials.footer')
+            <!-- end: FOOTER -->
+        </div>
+    </div>
+
+    <script src="{{ asset('vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('vendors/apexcharts/apexcharts.js') }}"></script>
+    <script src="{{ asset('js/pages/dashboard.js') }}"></script>
+
+    <script src="{{ asset('js/main.js') }}"></script>
+    <!-- Custom Page Script -->
+    @stack('script')
+</body>
+
+</html>
---
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -0,0 +1,57 @@
+<div id="sidebar" class="active">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+            <div class="d-flex justify-content-between">
+                <div class="logo">
+                    <a href="#">BookStore</a>
+                </div>
+                <div class="toggler">
+                    <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li class="sidebar-title">Menu</li>
+
+                <li class="sidebar-item active ">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-grid-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="sidebar-item ">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-bookmark"></i>
+                        <span>Manage Kategori</span>
+                    </a>
+                </li>
+                <li class="sidebar-item ">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-journal-bookmark"></i>
+                        <span>Manage Buku</span>
+                    </a>
+                </li>
+                <li class="sidebar-item ">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-people"></i>
+                        <span>List User Terdaftar</span>
+                    </a>
+                </li>
+                <li class="sidebar-item ">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-bag-check"></i>
+                        <span>Pemesanan Buku</span>
+                    </a>
+                </li>
+                <li class="sidebar-item ">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-box-arrow-right"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
